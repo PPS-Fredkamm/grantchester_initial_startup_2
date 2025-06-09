134c6af9df18cd58e74b6d2bd020d6269804dc53
Drew Senour <drew.senour@proactiveusa.com>
2025-06-09 12:10:33 -0400

    added logo scroller

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,12 +4,21 @@ import Footer from '../Footer/Footer';
 import SubBanner from '../SubBanner/SubBanner';
 import WebBanner from '../WebBanner/WebBanner';
 import SecondaryBanner from '../SecondaryBanner/SecondaryBanner';
+import LogoScroller from '../LogoScroller/LogoScroller';
+import dartmouth from '../../assets/Images/LogoScrollerImages/dartmouth.jpg'
+import drexel from '../../assets/Images/LogoScrollerImages/drexel.jpg'
+import yale from '../../assets/Images/LogoScrollerImages/yale.png'
+import USC from '../../assets/Images/LogoScrollerImages/USC.png'
+
+const logoList = [dartmouth, yale, USC, drexel];
+
 
 function Home() {
   return (
     <>       
       <SubBanner />
-      <WebBanner />           
+      <WebBanner />
+      <LogoScroller logos={logoList} speed={25} height={50} />           
       <SecondaryBanner />
       <div className="home-container">
         <h1>Welcome to Alumbiz</h1>
